e4cc47e1c1fcfa94909f095190b0f490f2e23d65
Akhil Vasudevan <39289866+AkhilVasudevan@users.noreply.github.com>
2020-09-24 09:23:14 -0700

    Version 1.1

--- model.py
+++ model.py
@@ -1,12 +1,17 @@
 import torch
 import torch.nn as nn
 
+#feeddorward neural network
 class NeuralNet(nn.Module):
     def __init__(self, input_size, hidden_size, num_classes):
         super(NeuralNet, self).__init__()
+        #layer1 
         self.l1 = nn.Linear(input_size, hidden_size) 
+        #layer2
         self.l2 = nn.Linear(hidden_size, hidden_size) 
+        #layer3
         self.l3 = nn.Linear(hidden_size, num_classes)
+        #activation function
         self.relu = nn.ReLU()
     
     def forward(self, x):
--- training.py
+++ training.py
@@ -35,32 +35,30 @@ all_words=[stem(w) for w in all_words if w not in ignore_words]
 all_words=sorted(set(all_words))
 tags=sorted(set(tags))
 
-x_train=[]
-y_train=[]
-for (pattern_sentence,tag) in xy:
-    bag=bag_of_words(pattern_sentence,all_words)
-    x_train.append(tag)
-    #crossentropyloss used
-    label=tags.index(tag)
+X_train = []
+y_train = []
+for (pattern_sentence, tag) in xy:
+    bag = bag_of_words(pattern_sentence, all_words)
+    X_train.append(bag)
+    label = tags.index(tag)
     y_train.append(label)
 
-x_train=np.array(x_train)
-y_train=np.array(y_train)
+X_train = np.array(X_train)
+y_train = np.array(y_train)
 
 # Hyper-parameters 
-num_epochs=1000
-batch_size=8
-learning_rate=0.001
-input_size=len(x_train[0])
-hidden_size=8
-output_size=len(tags)
-
+num_epochs = 1000
+batch_size = 8
+learning_rate = 0.001
+input_size = len(X_train[0])
+hidden_size = 8
+output_size = len(tags)
 
 class ChatDataset(Dataset):
 
     def __init__(self):
-        self.n_samples = len(x_train)
-        self.x_data = x_train
+        self.n_samples = len(X_train)
+        self.x_data = X_train
         self.y_data = y_train
 
     def __getitem__(self, index):
@@ -69,34 +67,32 @@ class ChatDataset(Dataset):
     def __len__(self):
         return self.n_samples
 
-dataSet=ChatDataset()
-#train bot
-train_loader=DataLoader(dataset=dataSet,
-                          batch_size=batch_size,
-                          shuffle=True,
-                          num_workers=0)
+dataset=ChatDataset()
+train_loader=DataLoader(dataset=dataset,batch_size=batch_size,shuffle=True,num_workers=0)
 
 device=torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-model=NeuralNet(input_size,hidden_size,output_size).to(device)
-# Loss and optimizer
+model=NeuralNet(input_size, hidden_size, output_size).to(device)
+
+#Loss and optimizer
 criterion=nn.CrossEntropyLoss()
-optimizer=torch.optim.Adam(model.parameters(),lr=learning_rate)
-# Train the model
+optimizer=torch.optim.Adam(model.parameters(), lr=learning_rate)
+
+#Train the model
 for epoch in range(num_epochs):
     for (words, labels) in train_loader:
-        words = words.to(device)
-        labels = labels.to(dtype=torch.long).to(device)
+        words=words.to(device)
+        labels=labels.to(dtype=torch.long).to(device)
         
-        # Forward pass
-        outputs = model(words)
-
-        loss = criterion(outputs, labels)
+        #Forward pass
+        outputs=model(words)
+        loss=criterion(outputs, labels)
         
-        # Backward and optimize
+        #Backward and optimize
         optimizer.zero_grad()
         loss.backward()
         optimizer.step()
 
+#storing trained data
 data = {
 "model_state": model.state_dict(),
 "input_size": input_size,
